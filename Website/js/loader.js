4fdbcd247685636e6f712f8958637a265d42565f
Jaap <jareerink@gmail.com>
2021-12-17 17:48:30 +0100

    cijfers groter, doorlink na animatie

--- Website/js/loader.js
+++ Website/js/loader.js
@@ -1,7 +1,16 @@
 const button = document.getElementById("load");
+const loading = document.getElementById("loading");
+const tienTal = document.getElementById("tienTal");
+const eenHeid = document.getElementById("eenHeid");
 
 button.addEventListener("click", function() {
     console.log("click");
-    document.getElementById("tienTal").style.transform = "translateY(-317px)";
-    document.getElementById("eenHeid").style.transform = "translateY(-668px)";
+    loading.style.visibility = "visible";
+    tienTal.style.transform = "translateY(-575px)";
+    eenHeid.style.transform = "translateY(-1207px)";
 },false);
+
+tienTal.addEventListener('transitionend', () => {
+    console.log("next page");
+    window.location.href = 'home.html';
+})
